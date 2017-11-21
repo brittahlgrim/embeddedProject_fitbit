3edf96d4476eb06142352ade5a3768661c8b7871
britt <britt.ahlgrim@gmail.com>
2017-11-21 02:03:17 -0600

    sending response to esp8266

--- LPC_fitbitApi/api/controllers/LPC_fitbitApiController.js
+++ LPC_fitbitApi/api/controllers/LPC_fitbitApiController.js
@@ -3,9 +3,39 @@ var mongoose = require('mongoose'),
 
 //returns only the activities for the current day
 exports.list_all_activities = function(req, res){
-	Activity.find({}, function(err, activity){
-		if(err)
-			res.send(err);
-		res.json(activity);
-	});
+	//todo: make a response to fitbit api here
+	var fitBitResponse = exampleFitBitResponse;
+	var responseParsed = JSON.parse(fitBitResponse);
+	console.log(responseParsed);
+	// Activity.find({}, function(err, activity){
+	// 	if(err)
+	// 		res.send(err);
+	// 	//todo: uncomment this once the fitbit api call is actually being made
+	// 	activity = exampleFitBitResponse;
+	// 	res.json(activity);
+	// });
 };
+
+var exampleFitBitResponse = {
+	"activities-steps":[
+        {"dateTime":"2014-09-05","value":1433}
+    ],
+    "activities-steps-intraday":{
+        "dataset":[
+            {"time":"00:00:00","value":0},
+            {"time":"00:01:00","value":0},
+            {"time":"00:02:00","value":0},
+            {"time":"00:03:00","value":0},
+            {"time":"00:04:00","value":0},
+            {"time":"00:05:00","value":287},
+            {"time":"00:06:00","value":287},
+            {"time":"00:07:00","value":287},
+            {"time":"00:08:00","value":287},
+            {"time":"00:09:00","value":287},
+            {"time":"00:10:00","value":0},
+            {"time":"00:11:00","value":0},
+        ],
+        "datasetInterval":1,
+        "datasetType": "minute"
+    }
+}
--- LPC_fitbitApi/server.js
+++ LPC_fitbitApi/server.js
@@ -1,4 +1,5 @@
-const express = require('express')
+const express = require('express');
+var request = require('request');
 const app = express();
 var port = process.env.PORT || 3000;
 var mongoose = require('mongoose');
@@ -11,16 +12,54 @@ mongoose.connect('mongodb://localhost/LPC_fitbitApidb', {useMongoClient: true});
 app.use(bodyParser.urlencoded({extended:true}));
 //todo: app.use(bodyParser.json);
 
-var routes = require('./api/routes/LPC_fitbitApiRoutes');
-
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.get('/activities', function(req, res){
-  Activity.find({}, function(err, activity){
-    if(err)
-      res.send(err);
-    res.json(activity);
+  var fitBitResponse = JSON.stringify(exampleFitBitResponse);
+  var responseParsed = JSON.parse(fitBitResponse);
+  var dataByHour = responseParsed['activities-steps-intraday'].dataset;
+  console.log(dataByHour);
+
+  dataByHour.forEach(function(hour){
+    console.log(hour.time);
+    console.log(hour.value);
+    setTimeout(function(){
+      request({
+        uri: 'http://192.168.4.1:80/?' + hour,
+        function(error, response, body) {
+        }
+      });
+    }, 3000);
+
+    console.log("hour:\r\n");
+    console.log(hour);
   });
+
+  res.json(dataByHour);
 });
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
+
+var exampleFitBitResponse = {
+  "activities-steps":[
+        {"dateTime":"2014-09-05","value":1433}
+    ],
+    "activities-steps-intraday":{
+        "dataset":[
+            {"time":"00:00:00","value":0},
+            {"time":"00:01:00","value":0},
+            {"time":"00:02:00","value":0},
+            {"time":"00:03:00","value":0},
+            {"time":"00:04:00","value":0},
+            {"time":"00:05:00","value":287},
+            {"time":"00:06:00","value":287},
+            {"time":"00:07:00","value":287},
+            {"time":"00:08:00","value":287},
+            {"time":"00:09:00","value":287},
+            {"time":"00:10:00","value":0},
+            {"time":"00:11:00","value":0},
+        ],
+        "datasetInterval":1,
+        "datasetType": "minute"
+    }
+};
